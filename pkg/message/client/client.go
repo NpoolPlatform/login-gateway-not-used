0c7662242f6244ba971b6ffa525f1e02528d9a28
fxnt <1330672764@qq.com>
2022-05-19 15:40:56 +0800

    delete mq example

--- pkg/message/client/client.go
+++ pkg/message/client/client.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"encoding/json"
 	"fmt"
 
 	msgcli "github.com/NpoolPlatform/go-service-framework/pkg/rabbitmq/client"
@@ -42,28 +41,4 @@ func Init() error {
 	myClients[constant.ServiceName] = sampleClient
 
 	return nil
-}
-
-func ConsumeExample(h func(*msg.Example) error) error {
-	examples, ok := myClients[constant.ServiceName].consumers[msg.QueueExample]
-	if !ok {
-		return fmt.Errorf("consumer is not constructed")
-	}
-
-	for d := range examples {
-		example := msg.Example{}
-		err := json.Unmarshal(d.Body, &example)
-		if err != nil {
-			return fmt.Errorf("parse message example error: %v", err)
-		}
-
-		if h != nil {
-			err = h(&example)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return fmt.Errorf("WE SHOULD NOT BE HERE")
 }
